0efc06ab17368bea3144bc11ec066134b17aa7f9
Isaac Almanza <isaac.almanza19@gmail.com>
2023-10-16 13:28:18 -0500

    feat: add weth bank

--- src/WETHBank/WETHBank.sol
+++ src/WETHBank/WETHBank.sol
@@ -1,10 +1,22 @@
 pragma solidity ^0.8.20;
 
-import "./IERC20Permit.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
-contract ERC20Bank {
+interface IERC20Permit is IERC20 {
+    function permit(
+        address owner,
+        address spender,
+        uint value,
+        uint deadline,
+        uint8 v,
+        bytes32 r,
+        bytes32 s
+    ) external;
+}
+
+contract WETHBank {
     IERC20Permit public immutable token;
-    mapping(address => uint) public balanceOf;
+    mapping(address => uint256) public balanceOf;
 
     constructor(address _token) {
         token = IERC20Permit(_token);
@@ -13,8 +25,8 @@ contract ERC20Bank {
     function depositWithPermit(
         address owner,
         address spender,
-        uint amount,
-        uint deadline,
+        uint256 amount,
+        uint256 deadline,
         uint8 v,
         bytes32 r,
         bytes32 s
@@ -24,12 +36,12 @@ contract ERC20Bank {
         balanceOf[spender] += amount;
     }
 
-    function deposit(uint _amount) external {
+    function deposit(uint256 _amount) external {
         token.transferFrom(msg.sender, address(this), _amount);
         balanceOf[msg.sender] += _amount;
     }
 
-    function withdraw(uint _amount) external {
+    function withdraw(uint256 _amount) external {
         balanceOf[msg.sender] -= _amount;
         token.transfer(msg.sender, _amount);
     }
---
+++ test/WETHBank.t.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "forge-std/console.sol";
+import "forge-std/Test.sol";
+
+import "../src/WETHBank/WETHBank.sol";
+import "../src/WETHBank/WETHBankExploit.sol";
+
+contract WETHBankTest is Test {
+    
+    address bob = address(0x1223);
+
+    function setUp() public {
+       
+    }
+
+
+
+}
