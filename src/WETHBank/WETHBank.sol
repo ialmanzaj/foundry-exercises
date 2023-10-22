efe8925e1ee5d7adac982f9b8c14c1b1e11bedcf
Isaac Almanza <isaac.almanza19@gmail.com>
2023-10-21 22:56:27 -0500

    feat: add WETH bank base contracts

--- src/WETHBank/WETHBank.sol
+++ src/WETHBank/WETHBank.sol
@@ -3,16 +3,20 @@ pragma solidity ^0.8.20;
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
 
 interface IERC20Permit is IERC20 {
-    function permit(
-        address owner,
-        address spender,
-        uint value,
-        uint deadline,
-        uint8 v,
-        bytes32 r,
-        bytes32 s
-    ) external;
+    function permit(address owner, address spender, uint256 value, uint256 deadline, uint8 v, bytes32 r, bytes32 s)
+        external;
 }
+
+/*
+Alice has 10 WETH and deposited 1 WETH into ERC20Bank.
+
+Thinking she might deposit more later, she has set inifinite approval on ERC20Bank.
+
+In other words, ERC20Bank can spend unlimited amount of WETH owned by Alice.
+
+1. get all WETH owned by alice
+2
+*/
 
 contract WETHBank {
     IERC20Permit public immutable token;
@@ -27,12 +31,16 @@ contract WETHBank {
         address spender,
         uint256 amount,
         uint256 deadline,
+        //signature params
         uint8 v,
         bytes32 r,
         bytes32 s
     ) external {
+        // approval with signature
         token.permit(owner, spender, amount, deadline, v, r, s);
+        // transfer tokens from owner to contract
         token.transferFrom(owner, address(this), amount);
+        // update balance
         balanceOf[spender] += amount;
     }
 
